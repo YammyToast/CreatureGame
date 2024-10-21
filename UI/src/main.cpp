b2088a40c9f74c06c116cd8ea07f7875e08e1dd4
YammyToast <cyanjamesmail@gmail.com>
2024-10-21 11:39:26 +0100

    UI cmake

---
+++ UI/src/main.cpp
@@ -0,0 +1,23 @@
+#include <cute.h>
+using namespace Cute;
+
+int main(int argc, char* argv[])
+{
+    // Create a window with a resolution of 640 x 480.
+    CF_Result result = make_app("Fancy Window Title", 0, 0, 0, 640, 480, CF_APP_OPTIONS_WINDOW_POS_CENTERED_BIT, argv[0]);
+    if (is_error(result)) {
+        printf("Error: %s\n", result.details);
+        return -1;
+    }
+
+    while (app_is_running())
+    {
+        app_update();
+        // All your game logic and updates go here...
+        app_draw_onto_screen();
+    }
+
+    destroy_app();
+
+    return 0;
+}
